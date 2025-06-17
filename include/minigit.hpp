3807227c3bb56add290af1d41acc4a0e1514d53d
fitsum yoseph <fitsumyoseph1@gmail.com>
2025-06-17 13:48:07 +0300

    public interface

---
+++ include/minigit.hpp
@@ -0,0 +1,11 @@
+#ifndef MINIGIT_HPP
+#define MINIGIT_HPP
+
+#include <string>
+
+// Function declarations for MiniGit operations
+void init();
+void add(const std::string& filename);
+
+
+#endif // MINIGIT_HPP
